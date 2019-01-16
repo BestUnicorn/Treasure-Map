10466af46fee614a28ea69097b1aae2e0c7a434d
Christian <chansen302@gmail.com>
2019-01-16 14:27:31 -0800

    Committing current changes as listed above but including other files that may cause merge conflict on my directory, but will resolve myself

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ require("./routes/htmlRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({force: true}).then(function() { // Using {force: true} as the parameter for sync will reset the database each time the server is started
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
