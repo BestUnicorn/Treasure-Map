9a782e456e6a4ec0e46fa35b6bf5fa814ab641b5
William Lan <wlan76@yahoo.com>
2019-01-18 10:37:18 -0800

    update

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("view"));
 
-<<<<<<< HEAD
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -22,21 +21,6 @@ require("./routes/htmlRoutes.js")(app);
 
 
 db.sequelize.sync({ force: true }).then(function() {
-=======
-// Use sessions to keep track of user login status
-app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Get routes for pages and API/Database
-// =============================================================
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({force: true}).then(function() { // Using {force: true} as the parameter for sync will reset the database each time the server is started
->>>>>>> origin/master
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
