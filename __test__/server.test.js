cc4817e8f34c9ac1d1edde3fd033c7de275b8819
William Lan <wlan76@yahoo.com>
2019-01-22 10:02:41 -0800

    update server.test.js

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -3,12 +3,11 @@
 var Nightmare = require("nightmare");
 
 describe("Treasure Map", function() {
-  // The default tests in mocha is 2 seconds.
-  // Extending it to 30 seconds to have time to load the pages
+  
   var signupPage = '#header__homepage';
 
   it("should require me to signup or login", function(done) {
-    // ID for the login button.
+   
     return Nightmare({
         show: true
       })
@@ -22,23 +21,25 @@ describe("Treasure Map", function() {
   .click("#signupPage")
   .wait(1000)
   
+  
+
   .screenshot("__test__/signupPage.png")
   
   .scrollTo(500, 0)
   
-      .evaluate(function() {
-        return document.title;
-      })
-      .end()
-      // Asset the title is as expected
-      .then(function(result) {
-        expect(result).toBeDefined();
-        done();
-      });
+  .evaluate(function() {
+    return document.title;
+    })
+  .end()
+  
+  .then(function(result) {
+    expect(result).toBeDefined();
+    done();
+    });
   }, 30000);
 
   it("should require me to signup or login", function(done) {
-    // ID for the login button.
+    
     return Nightmare({
         show: true
       })
@@ -46,27 +47,26 @@ describe("Treasure Map", function() {
   
   .click("#loginPage")
   .type("#loginEmail", "unicorns@testmail.com")
-      // Enter password.
-      .type("#loginPassword", "test")
-      // Take a screenshot of the login form.
-      .screenshot("__test__/login.png")
-      // Click login button. Always check if you've done this where necessary!
-      // It's easy to forget.
-      .click("#loginBtn")
+    
+  .type("#loginPassword", "test")
+    
+  .screenshot("__test__/login.png")
+      
+  .click("#loginBtn")
   
   
-      .evaluate(function() {
-        return document.title;
+  .evaluate(function () {
+     return document.title;
       })
-      .end()
-      // Asset the title is as expected
-      .then(function(result) {
-        expect(result).toBeDefined();
-        done();
-      })
-      .catch(function(err) {
-        console.log(err);
-        done();
-      });
+  .end()
+    
+  .then(function (result) {
+    expect(result).toBeDefined();
+      done();
+    })
+  .catch(function (err) {
+    console.log(err);
+      done();
+    });
   }, 30000);
 });
